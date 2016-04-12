fc64251f1ff2ec4baa31fd2ff07e622842185d6f
Stuart Roth <stuartaroth@gmail.com>
2016-04-11 18:39:29 -0600

    Started hash_maps for perl

--- hash_maps/example.pl
+++ hash_maps/example.pl
@@ -1,2 +1,25 @@
 use strict;
 use warnings;
+
+my %hash_map;
+
+$hash_map{"Arthur Dent"} = "Earth";
+$hash_map{"Ford Prefect"} = "Betelgeuse Seven";
+$hash_map{"Zaphod Beeblebrox"} = "Betelgeuse Five";
+$hash_map{Marvin} = "Sirius";
+
+my $present_value_1 = $hash_map{"Zaphod Beeblebrox"};
+print "$present_value_1\n";
+
+my $present_value_2 = $hash_map{Marvin};
+print "$present_value_2\n";
+
+my $missing_value_1 = $hash_map{Fenchurch};
+print "$missing_value_1\n";
+
+my $missing_value_2 = $hash_map{"Tricia McMillan"};
+print "$missing_value_2\n";
+
+foreach my $key (keys %hash_map) {
+    print "$key:$hash_map{$key}\n";
+}
