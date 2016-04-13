395bdb94557ee0df4f5f46ced89daa22aafc68c4
Stuart Roth <stuartaroth@gmail.com>
2016-04-13 00:28:04 -0600

    Added java and go to arrays_lists

--- arrays_lists/Example.java
+++ arrays_lists/Example.java
@@ -1,8 +1,63 @@
-import java.util.Arrays;
 import java.util.List;
+import java.util.ArrayList;
 
 class Example {
     public static void main(String[] args) {
+        List<String> arrayList = new ArrayList<String>() {{
+                add("B");
+                add("C");
+                add("D");
+                add("E");
+                add("F");
+                add("G");
+                add("H");
+        }};
+        // [ "B", "C", "D", "E", "F", "G", "H" ]
 
+        arrayList.add("I");
+        // [ "B", "C", "D", "E", "F", "G", "H", "I" ]
+
+        arrayList.add(0, "A");
+        // [ "A", "B", "C", "D", "E", "F", "G", "H", "I" ]
+
+        arrayList.remove(arrayList.size() - 1);
+        // [ "A", "B", "C", "D", "E", "F", "G", "H" ]
+
+        arrayList.remove(0);
+        // [ "B", "C", "D", "E", "F", "G", "H" ]
+
+        int length = arrayList.size();
+        // 7
+
+        String firstItem = arrayList.get(0);
+        // "B"
+
+        String lastItem = arrayList.get(arrayList.size() - 1);
+        // "H"
+
+        String nthItem = arrayList.get(3);
+        // "E"
+
+        int position = arrayList.indexOf("F");
+        // 4
+
+        int missingPosition = arrayList.indexOf("Z");
+        // -1
+
+        List<String> slicedArrayList = arrayList.subList(3, 5);
+        // [ "E", "F" ]
+
+        List<String> slicedArrayList2 = arrayList.subList(4, arrayList.size());
+        // [ "F", "G", "H" ]
+
+        List<String> arrayListToCombine = new ArrayList<String>() {{
+            add("I");
+            add("J");
+            add("K");
+        }};
+        // [ "I", "J", "L" ]
+
+        arrayList.addAll(arrayListToCombine);
+        // [ "B", "C", "D", "E", "F", "G", "H", "I", "J", "K" ]
     }
 }
--- arrays_lists/example.go
+++ arrays_lists/example.go
@@ -3,5 +3,58 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Arrays")
+	slice := []string{"B", "C", "D", "E", "F", "G", "H"}
+	// [ "B", "C", "D", "E", "F", "G", "H" ]
+
+	slice = append(slice, "I")
+	// [ "B", "C", "D", "E", "F", "G", "H", "I" ]
+
+	slice = append([]string{"A"}, slice...)
+	// [ "A", "B", "C", "D", "E", "F", "G", "H", "I" ]
+
+	slice = append(slice[:len(slice) - 1], slice[len(slice):]...)
+	// [ "A", "B", "C", "D", "E", "F", "G", "H" ]
+
+	slice = append(slice[:0], slice[1:]...)
+	// [ "B", "C", "D", "E", "F", "G", "H" ]
+
+	length := len(slice)
+	// 7
+
+	firstItem := slice[0]
+	// "B"
+
+	lastItem := slice[len(slice) - 1]
+	// "H"
+
+	nthItem := slice[3]
+	// "E"
+
+	sliceCopy := []string{}
+	sliceCopy = append(sliceCopy, slice...)
+	// [ "B", "C", "D", "E", "F", "G", "H" ]
+
+	slicedSlice := []string{}
+	slicedSlice = append(slicedSlice, slice[3:5]...)
+	// [ "E", "F" ]
+
+	slicedSlice2 := []string{}
+	slicedSlice2 = append(slicedSlice2, slice[4:]...)
+	// [ "F", "G", "H" ]
+
+	combinedSlice := []string{}
+	combinedSlice = append(combinedSlice, slice...)
+	combinedSlice = append(combinedSlice, []string{"I", "J", "K"}...)
+	// [ "B", "C", "D", "E", "F", "G", "H", "I", "J", "K" ]
+
+	fmt.Println(
+		length,
+		firstItem,
+		lastItem,
+		nthItem,
+		sliceCopy,
+		slicedSlice,
+		slicedSlice2,
+		combinedSlice,
+	)
 }
