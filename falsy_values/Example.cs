4518f61c00f0cf4cb4efcaa608b5582f07a4880e
Stuart Roth <stuartaroth@gmail.com>
2016-04-26 02:44:36 -0600

    Added C# to run_all and added a few other examples

---
+++ falsy_values/Example.cs
@@ -0,0 +1,7 @@
+public class Example
+{
+    public static void Main()
+    {
+        // C# does not support truthy/falsy values
+    }
+}
---
+++ hash_maps/Example.cs
@@ -0,0 +1,7 @@
+public class Example
+{
+    public static void Main()
+    {
+        
+    }
+}
