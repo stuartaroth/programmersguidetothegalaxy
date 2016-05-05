80d1bcf5ea8e43f620bb02101afba9712a233b8a
Corey Mayo <coremayo@gmail.com>
2016-05-05 09:43:20 -0400

    change last_nam to last_name

--- simple_class/example.py
+++ simple_class/example.py
@@ -1,17 +1,17 @@
 class Author:
-  def __init__(self, first_name, last_nam, year_born):
+  def __init__(self, first_name, last_name, year_born):
     self.first_name = first_name
-    self.last_nam = last_nam
+    self.last_name = last_name
     self.year_born = year_born
 
   def get_full_name(self):
-    return self.first_name + " " + self.last_nam
+    return self.first_name + " " + self.last_name
 
 author = Author("Douglas", "Adams", 1952)
-# Author { first_name: 'Douglas', last_nam: 'Adams', year_born: 1952 }
+# Author { first_name: 'Douglas', last_name: 'Adams', year_born: 1952 }
 
 author.first_name = "Doug"
-# Author { first_name: 'Doug', last_nam: 'Adams', year_born: 1952 }
+# Author { first_name: 'Doug', last_name: 'Adams', year_born: 1952 }
 
 fullName = author.get_full_name()
 # "Doug Adams"
