9334d22211c33e3df0943a4a74c007304cfec11c
Stuart Roth <stuartaroth@gmail.com>
2016-05-07 20:17:19 -0600

    Added groovy function file

--- simple_class/example.groovy
+++ simple_class/example.groovy
@@ -1,24 +1,30 @@
 class Author {
-  String firstName
-  String lastName
-  int yearBorn
+    String firstName
+    String lastName
+    int yearBorn
 
-  Author(String firstName, String lastName, int yearBorn) {
-    this.firstName = firstName
-    this.lastName = lastName
-    this.yearBorn = yearBorn
-  }
+    Author(String firstName, String lastName, int yearBorn) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.yearBorn = yearBorn
+    }
 
-  def getFullName() {
-    this.firstName + " " + this.lastName
-  }
+    def getFullName() {
+        this.firstName + " " + this.lastName
+    }
 }
 
-def author = new Author("Douglas", "Adams", 1952);
+def author = new Author("Douglas", "Adams", 1952)
 // Author { firstName: 'Douglas', lastName: 'Adams', yearBorn: 1952 }
 
-author.firstName = "Doug";
+author.firstName = "Doug"
 // Author { firstName: 'Doug', lastName: 'Adams', yearBorn: 1952 }
 
-def fullName = author.getFullName();
-// "Doug Adams"
+author.setLastName "A."
+// Author { firstName: 'Doug', lastName: 'A.', yearBorn: 1952 }
+
+def fullName = author.getFullName()
+// "Doug A."
+
+def yearBorn = author.getYearBorn()
+// 1952
