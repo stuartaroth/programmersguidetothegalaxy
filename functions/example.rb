3b31c7b636725608e83817af026fb06f4e189d17
Paolo Ibarra <jpibarra1130@gmail.com>
2016-05-17 08:05:04 +0800

    Added sample function with default value parameter for ruby

--- functions/example.rb
+++ functions/example.rb
@@ -6,8 +6,18 @@ def greet_person(name)
   return "Hello " + name
 end
 
+def get_final_price(amount, tax: 0.05)
+  return amount * (1 + tax)
+end
+
 i = multiply(6, 9)
 # 54
 
 s = greet_person("Ford Prefect")
 # "Hello Ford Prefect"
+
+final_price = get_final_price(100.0)
+# 105.0
+
+final_price = get_final_price(100.0, tax: 0.07)
+# 107.0
