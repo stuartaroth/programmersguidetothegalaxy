8740bc69e4433c9493c7d801419cbd2fe386d616
Paolo Ibarra <jpibarra1130@gmail.com>
2016-05-17 08:01:49 +0800

    Added sample function with default values for swift

--- functions/Example.swift
+++ functions/Example.swift
@@ -6,8 +6,18 @@ func greetPerson(name: String) -> String {
   return "Hello " + name
 }
 
+func getFinalPrice(amount: Double, tax: Double = 0.05) -> Double {
+    return amount * (1 + tax)
+}
+
 var i = multiply(6, y: 9)
 // 54
 
 var s = greetPerson("Ford Prefect")
 // "Hello Ford Prefect"
+
+var final_price = getFinalPrice(100)
+// 105
+
+final_price = getFinalPrice(100.0, tax:0.07)
+// 107
