a0419322832b8598b563fb5a64e4d9dba266f73f
Basil Arackal <basil4mj@gmail.com>
2016-06-11 10:24:57 +0530

    Fixed Indentation for hash maps iteration of C++ example

--- hash_maps_iteration/example.cpp
+++ hash_maps_iteration/example.cpp
@@ -5,16 +5,15 @@
 using namespace std;
 
 int main(){
+    map<string, int> hashMap; 
+    hashMap.insert(pair<string, int>("The Hitchhiker's Guide to the Galaxy", 1979));
+    hashMap.insert(pair<string, int>("The Restaurant at the End of the Universe", 1980));
+    hashMap.insert(pair<string, int>("Life, the Universe and Everything", 1982));
+    hashMap.insert(pair<string, int>("So Long, and Thanks for All the Fish", 1984));
+    hashMap.insert(pair<string, int>("Mostly Harmless", 1992));
 
-        map<string, int> hashMap; 
-        hashMap.insert(pair<string, int>("The Hitchhiker's Guide to the Galaxy", 1979));
-        hashMap.insert(pair<string, int>("The Restaurant at the End of the Universe", 1980));
-        hashMap.insert(pair<string, int>("Life, the Universe and Everything", 1982));
-        hashMap.insert(pair<string, int>("So Long, and Thanks for All the Fish", 1984));
-        hashMap.insert(pair<string, int>("Mostly Harmless", 1992));
-
-        for (map<char,int>::iterator i=hashMap.begin(); i!=hashMap.end(); ++i)
-    		cout << i->first << " " << i->second << '\n';
+    for (map<char,int>::iterator i=hashMap.begin(); i!=hashMap.end(); ++i)
+        cout << i->first << " " << i->second << '\n';
 
 	return 0;
 }
